cff34a290d8015fe5548ea930b2be49b42ca6516
cforgione22 <crystal.forgione22@gmail.com>
2024-09-11 15:58:55 -0400

    getter and setters

---
+++ Box.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ *  Box child class
+ */
+
+
+public class Box extends Shape {
+
+    public Box(Point p1, Point p2, Color color) {
+
+        super(p1, p2, color);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+}
---
+++ Shape.java
@@ -0,0 +1,69 @@
+import java.awt.*;
+/**
+ *  abstract parent class
+ */
+
+public abstract class Shape {
+
+    private Point p1;
+    private Point p2;
+    private Color color;
+    private int lineWidth = 2;
+
+    public Shape( )
+    {
+        p1 = new Point(0,0);
+        p2 = new Point(0,0);
+        color = Color.BLUE;
+    }
+
+    public Shape(Point p1, Point p2, Color c)
+    {
+        setP1(p1);
+        setP2(p2);
+        setColor(c);
+    }
+
+    public void setP1(Point p1)
+    {
+        this.p1 = p1;
+    }
+
+
+    public Point getP1 ()
+    {
+        return p1;
+    }
+
+    public void setP2(Point p2)
+    {
+        this.p2 = p2;
+    }
+
+    public Point getP2 ()
+    {
+        return p2;
+    }
+
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+
+    public Color getColor(Color red)
+    {
+        return color;
+    }
+
+    public int getLineWidth() {
+        return lineWidth;
+    }
+
+    public void setLineWidth(int lineWidth) {
+        this.lineWidth = lineWidth;
+    }
+
+    public abstract void draw(Graphics g);
+}
+
+
